b5d69c6aeaf4526a50f09c490cfeeba023ead9cc
Alex Prochazka <alexprosd@gmail.com>
2024-12-09 03:56:06 -0800

    add graph traversal logic, connect logic to parser

--- AlLogic.py
+++ AlLogic.py
@@ -1,24 +1,25 @@
-# from textx import metamodel_from_file
-# allogic_mm = metamodel_from_file('AlLogic.tx')
-# alllogic_model = allogic_mm.model_from_file('test.al')
+from collections import deque
+from textx import metamodel_from_file
 
-defined_circuits = {}
 
+defined_gates = {}
+declared_circuits = {}
 
 class Gate:
-    def __init__(self, num_inputs, num_outputs):
+    def __init__(self, identifier, num_inputs, num_outputs):
+        self.identifier = identifier
         self.inputs = num_inputs
         self.outputs = num_outputs
-
+        defined_gates.update({identifier:self})
 
 class SimpleGate(Gate):
-    def __init__(self, num_inputs, num_outputs, truth_table):
-        super().__init__(num_inputs, num_outputs)
+    def __init__(self, identifier, num_inputs, num_outputs, truth_table):
+        super().__init__(identifier, num_inputs, num_outputs)
         self.truth_table = truth_table
 
 class ComplexGate(Gate):
-    def __init__(self, num_inputs, num_outputs):
-        super().__init__(num_inputs, num_outputs)
+    def __init__(self, identifier, num_inputs, num_outputs):
+        super().__init__(identifier, num_inputs, num_outputs)
         self.components = {}
         self.connections = []
 
@@ -39,10 +40,11 @@ class CircuitInstance:
 
 
         if isinstance(gate, ComplexGate):
-            # Initialize sub-circuits
+            # Initialize sub-circuits recursively
             for identifier, sub_gate in gate.components.items():
                 self.sub_circuits[identifier] = CircuitInstance(identifier, sub_gate)
 
+            # Build graph based on connections specified in ComplexGate
             for from_gate, from_node, to_gate, to_node in gate.connections:
                 circuit_from = self if from_gate == "this"  else self.sub_circuits.get(from_gate)
                 circuit_to = self if to_gate == "this" else self.sub_circuits.get(to_gate)
@@ -55,9 +57,15 @@ class CircuitInstance:
                 circuit_from = self if from_gate == "this"  else self.sub_circuits.get(from_gate)
                 circuit_to = self if to_gate == "this" else self.sub_circuits.get(to_gate)
                 node_object_from = circuit_from.input_nodes[from_node[1]] if from_node[0] == 'in' else circuit_from.output_nodes[from_node[1]]
+        
+        #evaluation of  to reach a valid initial state
+        self.evaluate()
 
     def __str__(self):
-        return f'{self.name}, {super().__str__()}'
+        inputs = ', '.join(str(node.value) for node in self.input_nodes)
+        outputs = ', '.join(str(node.value) for node in self.output_nodes)
+        return f"Circuit '{self.name}' (Type: {self.type.identifier}): Inputs: [{inputs}] Outputs: [{outputs}]"
+
 
 
 
@@ -76,28 +84,45 @@ class CircuitInstance:
         if isinstance(self.type, SimpleGate):
             # Evaluate using truth table
             input_values = tuple(node.value for node in self.input_nodes)
-            output_values = self.type.truth_table.get(input_values, (0,) * self.type.outputs)
-            for node, value in zip(self.output_nodes, output_values):
-                node.set_value(value)
+            output_values = self.type.truth_table.get(input_values)
+            for i in range(len(output_values)):
+                self.output_nodes[i].value = output_values[i]
         elif isinstance(self.type, ComplexGate):
-            # Propagate logic through sub-components
-            for connection in self.type.connections:
-                output_gate, output_node, input_gate, input_node = connection
-                source_value = self.sub_circuits[output_gate].output_nodes[output_node].value
-                self.sub_circuits[input_gate].input_nodes[input_node].set_value(source_value)
+            # Propagation logic
+            #instantiate a queue
+            node_queue = deque()
+            #add input dependents to the queue
+            for node in self.input_nodes:
+                node.initialized = True
+                for dependent in node.dependents:
+                    node_queue.append(dependent)
+            while node_queue:
+                current = node_queue.popleft()
+                previous_value = current.value
+                #evaluate current node value
+                #if a node has a source, update its value to the same as its source
+                if current.source is not None:
+                    current.value = current.source.value
+                #otherwise, if node is an output of a SimpleGate, update its value based on node's owner's input values at that moment
+                else:
+                    upstream_inputs = tuple(node.value for node in current.owner.input_nodes) #grabs current inputs associated with the owner circuit
+                    current_index = current.owner.output_nodes.index(current) #grabs index of current node in owner circuit
+                    current.value = current.owner.type.truth_table.get(upstream_inputs)[current_index] #grabs specific output from all of them
 
-            # Evaluate all sub-circuits
-            for sub_circuit in self.sub_circuits.values():
-                sub_circuit.evaluate()
-
-            # Update top-level outputs
-            for i, output_node in enumerate(self.output_nodes):
-                dependent_gate = self.sub_circuits[output_node.source]
-                output_node.set_value(dependent_gate.get_current_outputs()[i])
+                #queue dependents if circuit is being initialized or has been changed
+                if not current.initialized or current.value != previous_value:
+                    for dependent in current.dependents:
+                        node_queue.append(dependent)
+                    #if a node is an input to a simplegate, add the simpleGate's outputs to the queue
+                    if current in current.owner.input_nodes:
+                        for dependent in current.owner.output_nodes:
+                            node_queue.append(dependent)
+                    current.initialized = True
 
 # Node can have only 1 source, but multiple dependents
 class Node:
     def __init__(self, gate: CircuitInstance):
+        self.initialized = False
         self.value = 0
         self.source = None
         self.dependents = []
@@ -135,72 +160,73 @@ not_gate_truth_table = {
     (0,): (1,),
     (1,): (0,)
 }
-and_gate = SimpleGate(2, 1, and_gate_truth_table)
-defined_circuits.update({'AND':and_gate})
-or_gate = SimpleGate(2, 1, or_gate_truth_table)
-defined_circuits.update({'OR':or_gate})
-not_gate = SimpleGate(1, 1, not_gate_truth_table)
-defined_circuits.update({'NOT':not_gate})
 
-and3_gate = ComplexGate(3, 1)
-and3_gate.add_component(and_gate, "and0")
-and3_gate.add_component(and_gate, "and1")
-and3_gate.connect("this", ('in',0), "and0", ('in',0))
-and3_gate.connect("this", ('in',1), "and0", ('in',1))
-and3_gate.connect("and0", ('out',0), "and1", ('in',0))
-and3_gate.connect("this", ('in',2), "and1", ('in',1))
-and3_gate.connect("and1", ('out',0), "this", ('out',0))
-defined_circuits.update({'AND3':and3_gate})
-test = CircuitInstance('test', defined_circuits.get('AND3'))
+and_gate = SimpleGate('AND', 2, 1, and_gate_truth_table)
+or_gate = SimpleGate('OR', 2, 1, or_gate_truth_table)
+not_gate = SimpleGate('NOT', 1, 1, not_gate_truth_table)
 
-print('\n\n\n\n\nWeirdgate:')
+# sr_latch = ComplexGate('SR_LATCH', 2, 1)
+# sr_latch.add_component(or_gate, 'or')
+# sr_latch.add_component(and_gate, 'and')
+# sr_latch.add_component(not_gate, 'not')
+# sr_latch.connect("this", ('in', 0), "or", ('in', 1))
+# sr_latch.connect("this", ('in', 1), "not", ('in', 0))
+# sr_latch.connect("not", ('out', 0), "and", ('in', 1))
+# sr_latch.connect("or", ('out', 0), "and", ('in', 0))
+# sr_latch.connect("and", ('out', 0), "or", ('in', 0))
+# sr_latch.connect("and", ('out', 0), "this", ('out', 0))
+# sr = CircuitInstance('sr', sr_latch)
+# print(sr)
+# sr.update_inputs((1,0))
+# print(sr)
+# sr.update_inputs((0,0))
+# print(sr)
+# sr.update_inputs((1,0))
+# print(sr)
+# sr.update_inputs((0,0))
+# print(sr)
+# sr.update_inputs((0,1))
+# print(sr)
 
-weird_gate = ComplexGate(3,1)
-weird_gate.add_component(and3_gate, 'and')
-# weird_gate.add_component(not_gate, 'not')
-weird_gate.connect("this", ('in', 0), "and", ('in', 0))
-weird_gate.connect("this", ('in', 1), "and", ('in', 1))
-weird_gate.connect("this", ('in', 2), "and", ('in', 2))
-# weird_gate.connect("this", ('in', 3), "not", ('in', 0))
-weird_gate.connect("and", ('out', 0), "this", ('out', 0))
-# weird_gate.connect("not", ('out', 0), "this", ('out', 1))
+class AlLogic:
 
-test1 = CircuitInstance('test1', weird_gate)
+    def interpret(self, model):
 
+        # model is an instance of Program
+        for instruction in model.instructions:
 
-print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
-print(test)
-# class AlLogic:
-#     def __init__(self):
-#         circuits = list
-#         declarations = list
+            if instruction.__class__.__name__ == "Definition":
+                gate_name = instruction.name
+                num_inputs = instruction.inputs
+                num_outputs = instruction.outputs
+                gate = ComplexGate(identifier=gate_name, num_inputs=num_inputs, num_outputs=num_outputs)
+                defined_gates.update({gate_name: gate})
 
-#     def interpret(self, model):
-
-#         # model is an instance of Program
-#         for instruction in model.instructions:
-
-#             if instruction.__class__.__name__ == "Definition":
-#                 circuit_name = instruction.name
-#                 num_inputs = instruction.inputs
-#                 num_outputs = instruction.outputs
-#                 circuit = Circuit(num_inputs=num_inputs, num_outputs=num_outputs)
-#                 defined_circuits.update({circuit_name, circuit})
-
-#                 for inclusion in model.inclusions:
-#                     circuit.add_circuit(name = inclusion.instance, circuit = inclusion.gate)
+                for inclusion in instruction.inclusions:
+                    gate.add_component(defined_gates.get(inclusion.gate), inclusion.identifier)
                 
-#                 for connection in model.connections:
-#                     circuit.connect() #todo make sure these parameters are enough and descriptive
+                for connection in instruction.connections:
+                    source_node = connection.source
+                    target_node = connection.target
+                    gate.connect(source_node.instance, (source_node.side, source_node.index), target_node.instance, (target_node.side, target_node.index))
             
-#             elif instruction.__class__.__name__ == "Execution":
-#                 print('hi')
+            elif instruction.__class__.__name__ == "Declaration":
+                circuit = CircuitInstance(instruction.identifier, defined_gates.get(instruction.gate))
+                declared_circuits.update({instruction.identifier: circuit})
             
-#             elif instruction.__class__.__name__ == "Execution":
-#                 target = instruction.circuit
-#                 values = [i for i in instruction.values]
-#                 circuit = defined_circuits.get(target)
-#                 circuit.execute(values)
+            elif instruction.__class__.__name__ == "Execution":
+                values = tuple([i for i in instruction.values])
+                circuit = declared_circuits.get(instruction.circuit)
+                circuit.update_inputs(values)
+                print(circuit)
 
-# logic = AlLogic()
-# logic.interpret(alllogic_model)
+allogic_mm = metamodel_from_file('AlLogic.tx')
+alllogic_model = allogic_mm.model_from_file('test.al')
+logic = AlLogic()
+logic.interpret(alllogic_model)
+
+
+
+
+
+
--- test.al
+++ test.al
@@ -1,14 +1,14 @@
-define halfAdder(2:2){
+define AND3(3:1){
+    include AND:and0
     include AND:and1
-    include XOR:xor1
 
-    this.in[0] -> and1.in[0]
-    this.in[1] -> and1.in[1]
-    this.in[0] -> xor1.in[0]
-    this.in[1] -> xor1.in[1]
+    this.in[0] -> and0.in[0]
+    this.in[1] -> and0.in[1]
+    this.in[2] -> and1.in[0]
+    and0.out[0] -> and1.in[1]
     and1.out[0] -> this.out[0]
-    xor1.out[0] -> this.out[1]
 }
 
-declare halfAdder: test
-test[1,1]
+declare AND3: test
+test[0,0,1]
+test[1,1,1]
